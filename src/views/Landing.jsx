701b17d2ae004486abd035032d5cf5d54dc0f7c4
Juan Javier Geringer <juanjaviergeringer@gmail.com>
2024-05-08 21:31:21 -0300

    borre la prueba de redux

--- src/views/Landing.jsx
+++ src/views/Landing.jsx
@@ -1,33 +1,12 @@
 import React from 'react'
 
-/* dependencias */
-import {useEffect,useState} from "react";
-import {useDispatch, useSelector} from "react-redux";
-
-/* actions */
-import {getDrivers} from "../redux/actions"
-
 const Landing = () => {
-  const dispatch = useDispatch();
-  /* Estado global */
-  const allDrivers = useSelector((state)=> state.allDrivers);
-
-
-
-  //Primera renderizacion
-  useEffect(() => {
-    // Verifica si la lista de conductores ya está cargada en el estado global de Redux
-    if (!allDrivers.length) {
-      // Si no está cargada, realiza la carga inicial
-      dispatch(getDrivers());
-      
-    }
-  }, [dispatch, allDrivers]);
+ 
 
   return (
     <div className='min-h-screen'>
       <h1>Landing</h1>
-      <button onClick={()=>console.log(allDrivers)} className='bg-primary text-secondary-900'>GetDrivers</button>
+      
     </div>
 
   )
