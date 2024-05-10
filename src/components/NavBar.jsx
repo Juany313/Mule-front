8bbcff1b9019aec29cd28274fbb8c33a9085fbf0
daniloalvarez7 <alvarez.danilo1234@gmail.com>
2024-05-10 00:13:30 -0300

    landing, about y navbar

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,64 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+const NavBar = () => {
+  return (
+    <nav className="bg-p300 p-4">
+      <div className="mx-auto">
+        <div className="flex justify-between items-center">
+          <div className="text-white font-bold pl-9 text-4xl pt-1">
+            <NavLink to="/">MULE</NavLink>
+          </div>
+          <div className="flex space-x-4">
+            <NavLink
+              to="/header"
+              className="text-white hover:text-gray-300 p-2"
+              activeClassName="text-blue-500"
+              exact
+            >
+              Inicio
+            </NavLink>
+            <NavLink
+              to="/header/about"
+              className="text-white hover:text-gray-300 p-2"
+              activeClassName="text-blue-500"
+            >
+              Nosotros
+            </NavLink>
+            <NavLink
+              to="/header/service"
+              className="text-white hover:text-gray-300 p-2"
+              activeClassName="text-blue-500"
+            >
+              Servicios
+            </NavLink>
+            <NavLink
+              to="/agencias"
+              className="text-white hover:text-gray-300 p-2"
+              activeClassName="text-blue-500"
+            >
+              Agencias
+            </NavLink>
+            <NavLink
+              to="/contacto"
+              className="text-white hover:text-gray-300 p-2"
+              activeClassName="text-blue-500"
+            >
+              Contacto
+            </NavLink>
+            <NavLink
+              to="/cotizar"
+              className="text-white hover:text-gray-300 p-2"
+              activeClassName="text-blue-500"
+            >
+              Cotizar
+            </NavLink>
+            <button className="bg-s300 py-2 px-4 rounded">Ingresar</button>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default NavBar;
--- src/views/Landing.jsx
+++ src/views/Landing.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from "react";
+import landing from "../assets/landing.jpg";
+import { NavLink } from "react-router-dom";
 
 const Landing = () => {
   return (
-    <div>Landing</div>
-  )
-}
+    <div
+      className="bg-cover bg-center h-screen"
+      style={{ backgroundImage: `url(${landing})` }}
+    >
+      <div className="grid grid-cols-3 grid-rows-12">
+        <NavLink
+          to="/header"
+          className="text-7xl text-white font-bold text-center col-start-2 row-start-5"
+        >
+          MULE
+        </NavLink>
+      </div>
+    </div>
+  );
+};
 
-export default Landing
+export default Landing;
