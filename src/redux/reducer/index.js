7611ae9bfcc530b9344558f3b90fbf3b06ae64f5
Emilse <memilseo@gmail.com>
2024-05-13 21:06:53 -0300

    cambios

--- src/redux/reducer/index.js
+++ src/redux/reducer/index.js
@@ -1,4 +1,4 @@
-import { GET_ALL_ORDERS, GET_ALL_USERS, GET_ORDER_ID, GET_USER_DETAIL, FILTER_VALUES } from "../actions";
+import { GET_ALL_ORDERS, GET_ALL_USERS, GET_ORDER_ID, GET_USER_DETAIL } from "../actions";
 
 
 let initialState = {
@@ -11,19 +11,39 @@ let initialState = {
 
 function rootReducer(state = initialState, action){
     switch (action.type) {
-
-        
-        case GET_DRIVERS:
+        case GET_ALL_USERS:
             return {
                 ...state,
-                allDrivers: action.payload,
-              };
-        
-            
+                allUsers: action.payload,
+            };
+
+        case GET_USER_DETAIL:
+            return {
+                ...state,
+                userDetail: action.payload,
+            };
+
+        case GET_ALL_ORDERS:
+            return {
+                ...state,
+                allOrders: action.payload,
+            }; 
+
+        case GET_ORDER_ID:
+            return {
+                ...state,
+                orderDetail: action.payload
+            }; 
+
+        case  FILTER_VALUES:
+            return {
+                ...state,
+                allOrders:action.payload
+            }
+           
         default:
             return {...state};
     }
 }
 
 export default rootReducer;
-
