087ca28dc2f7a54646e198ba51fe1f768db2a710
Juan Javier Geringer <juanjaviergeringer@gmail.com>
2024-05-15 22:07:28 -0300

    empezando con auth

--- src/main.jsx
+++ src/main.jsx
@@ -5,8 +5,19 @@ import "./index.css";
 import { store } from "./redux/store";
 import { Provider } from "react-redux";
 
+/* authentication */
+import {Auth0Provider} from '@auth0/auth0-react';
+
+console.log(
+  process.env.REACT_APP_AUTH0_DOMAIN
+);
+
+//! 12 MINUTOS DEL ARCHIVO DE fAZT CODE
+
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <Provider store={store}>
-    <App />
-  </Provider>
+  <Auth0Provider>
+      <Provider store={store}>
+        <App />
+      </Provider>
+  </Auth0Provider>
 );
