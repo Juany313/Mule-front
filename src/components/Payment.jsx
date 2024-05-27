8c832f5ba9b9e50328013772211453dff0ab5254
Luis Gómez <luiscgr97@gmail.com>
2024-05-27 14:59:30 -0500

    Cambios de dani de vuelta

--- src/components/Payment.jsx
+++ src/components/Payment.jsx
@@ -1,51 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
+import { useLocation } from "react-router-dom";
 import { initMercadoPago, Wallet } from "@mercadopago/sdk-react";
 import axios from "axios";
-import { useState } from "react";
-
-const products = [
-  {
-    id: 1,
-    type_shipments: "Puerta a Puerta",
-    measure: "Pequeño",
-    city_receiver: "Córdoba",
-    city_transmiter: "Santa Fe",
-    price: 2500,
-    quantity: 1,
-  },
-  {
-    id: 2,
-    type_shipments: "Puerta a Sucursal",
-    measure: "Mediano",
-    city_receiver: "Buenos Aires",
-    city_transmiter: "Corrientes",
-    price: 5000,
-    quantity: 1,
-  },
-  {
-    id: 3,
-    type_shipments: "Sucursal a Puerta",
-    measure: "Grande",
-    city_receiver: "Córdoba",
-    city_transmiter: "Entre Ríos",
-    price: 7500,
-    quantity: 1,
-  },
-];
 
 const Payment = () => {
   const [preferenceId, setPreferenceId] = useState(null);
   const location = useLocation();
   const { orderData } = location.state;
 
-  initMercadoPago("APP_USR-cfcbc101-b2d1-447a-9572-dc7a2935f197", {
+  initMercadoPago("TEST-fba43614-ef7f-45c0-8443-fc699301ff2c", {
     locale: "es-AR",
   });
 
-  const handlePay = async (id, title, quantity, unit_price) => {
+  const handlePay = async () => {
     try {
       const response = await axios.post("http://localhost:3000/payments/", {
-        id: orderData.id,
+        id: orderData.typeShipmentId,
         title:
           orderData.typeShipmentId === 1
             ? "Sucursal a puerta"
@@ -80,6 +50,7 @@ const Payment = () => {
             number: orderData.cedula_claimant,
           },
         },
+        pay_method: orderData.pay_method,
         shipments: {
           receiver_address: {
             street_name: orderData.address_receiver,
@@ -96,46 +67,58 @@ const Payment = () => {
 
   return (
     <div className="container mx-auto p-4">
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-        {products.map((elem, index) => (
-          <div
-            key={index}
-            className="bg-white rounded-xl shadow-lg p-4 transform hover:scale-105 transition-transform duration-300"
-          >
+      {orderData && (
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+          <div className="bg-white rounded-xl shadow-lg p-4 transform hover:scale-105 transition-transform duration-300">
             <h1 className="font-bold text-lg">
-              Tipo de envío: {elem.type_shipments}
+              Tipo de envío:{" "}
+              {orderData.typeShipmentId === 1
+                ? "Sucursal a puerta"
+                : orderData.typeShipmentId === 2
+                ? "Sucursal a sucursal"
+                : orderData.typeShipmentId === 3
+                ? "Puerta a sucursal"
+                : orderData.typeShipmentId === 4
+                ? "Puerta a Puerta"
+                : null}
             </h1>
-            <h2 className="text-md">Tamaño: {elem.measure}</h2>
+            <h2 className="text-md">
+              Tamaño:{" "}
+              {orderData.measureId === 1
+                ? "Pequeño"
+                : orderData.measureId === 2
+                ? "Mediano"
+                : orderData.typeShipmentId === 3
+                ? "Grande"
+                : null}
+            </h2>
             <label className="block mt-2">Remitente:</label>
-            <h4 className="text-sm">{elem.city_transmiter}</h4>
+            <h4 className="text-sm">{orderData.city_transmiter}</h4>
             <label className="block mt-2">Destinatario:</label>
-            <h4 className="text-sm">{elem.city_receiver}</h4>
-            <label className="block mt-2">Precio:</label>
-            <h3 className="text-md font-semibold">{elem.price}</h3>
-
-            <button
-              className="mt-4 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-700"
-              onClick={() =>
-                handlePay(
-                  elem.id,
-                  elem.type_shipments,
-                  elem.quantity,
-                  elem.price
-                )
-              }
-            >
-              PAGAR
-            </button>
+            <h4 className="text-sm">{orderData.city_receiver}</h4>
+            <label className="block mt-2">Peso:</label>
+            <h4 className="text-sm">{orderData.weight}kg</h4>
+            <label className="block mt-2">Declarado:</label>
+            <h4 className="text-sm">{orderData.declared_value}$</h4>
+            <label className="block mt-2">Método de pago:</label>
+            <h4 className="text-sm">{orderData.pay_method}</h4>
+            <img
+              src={orderData.product_image}
+              alt="Producto"
+              className="mt-4"
+            />
           </div>
-        ))}
+        </div>
+      )}
+      <div className="p-4">
+        <button
+          onClick={() => handlePay(1, "Pieza", 1, 10)}
+          className="bg-s300 text-black uppercase font-bold text-sm w-full py-3 px-4 rounded-lg hover:text-gray-100 transition-colors"
+        >
+          Pagar
+        </button>
       </div>
-      <div id="wallet_container">
-        {preferenceId && (
-          <div className="mt-8">
-            <Wallet initialization={{ preferenceId: preferenceId }} />
-          </div>
-        )}
-      </div>
+      {preferenceId && <Wallet initialization={{ preferenceId }} />}
     </div>
   );
 };
