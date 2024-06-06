6017cc3aa04cc96517ced415b298d75c520cd656
Calvin Ulis Howard Howard <Calvin.uhh@gmail.com>
2024-06-05 19:00:59 -0500

    test nuevas credenciales deploy 2

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import { Auth0Provider } from "@auth0/auth0-react";
 import App from "./App";
@@ -11,12 +11,12 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-    <Auth0Provider
+      <Auth0Provider
         domain="dev-1m7fdf5yimzufwoe.us.auth0.com"
-        clientId="CxcvRJYL4FbKMOGuGsV3kwvaGzoPqkP7"
-    authorizationParams={{
-      redirect_uri: "https://mule-front.onrender.com/auth/dashboard"
-    }}
+        clientId="5fA00kciIw03oyxcwwmPnHAh9YgZvIQQ"
+        authorizationParams={{
+          redirect_uri: "https://mule-front.onrender.com//auth/dashboard",
+        }}
       >
         <App />
       </Auth0Provider>
